c3a015b3daf5d3cee26cbcfc5890629171c01b3c
diwadeep <141811766+diwadeep@users.noreply.github.com>
2023-08-14 12:51:47 +0530

    8 files modified, 2 added, 1 deleted

--- src/app/modules/quiz-app-module/components/question/question.component.ts
+++ src/app/modules/quiz-app-module/components/question/question.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit, OnChanges, AfterViewInit {
 
--- src/app/modules/quiz-app-module/quiz-app-module.module.ts
+++ src/app/modules/quiz-app-module/quiz-app-module.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuizModuleRoutingModule } from './quiz-module-routing.module';
-import { QuizComponent } from './components/quiz/quiz.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { QuestionComponent } from './components/question/question.component';
 import { ResultComponent } from './components/result/result.component';
+import { QuizEntryComponent } from './components/quiz-entry/quiz-entry.component';
+import { QuizAppModuleRoutingModule } from './quiz-app-module.routing.module';
 
 
 @NgModule({
   declarations: [
-    QuizApp,
+    QuizEntryComponent,
     QuestionComponent,
     ResultComponent
   ],
   imports: [
     CommonModule,
-    QuizModuleRoutingModule,
+    QuizAppModuleRoutingModule,
     ReactiveFormsModule
   ]
 })
